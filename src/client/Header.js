c12153955f412705c485fae513bea9fb35ff61cd
jaimakena <jaimakena@gmail.com>
2021-10-10 15:58:59 +0200

    src/client/Header.js

--- src/client/Header.js
+++ src/client/Header.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
-import { NavLink, Link } from "react-router-dom";
+import React, {useState} from "react";
+import {NavLink, Link} from "react-router-dom";
 
 function Header() {
-const[click, setClick] = useState(false);
+    const [click, setClick] = useState(false);
 
-const handleClick = () => setClick(!click);
-const closeMenu = () => setClick(false);
-  return (
-    <>
-    <nav className="navbar">
-      <div className="navbar-container">
-      <NavLink exact activeClassName="active" to="/">
-        Home | 
-      </NavLink>
-      <NavLink activeClassName="active" to="/meals">
-        Meals | 
-      </NavLink>
-      </div>
-    </nav>
-    </>
-  );
+    const handleClick = () => setClick(!click);
+    const closeMenu = () => setClick(false);
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <NavLink exact activeClassName="active" to="/">
+                        Home |
+                    </NavLink>
+                    <NavLink activeClassName="active" to="/meals">
+                        Meals |
+                    </NavLink>
+                </div>
+            </nav>
+        </>
+    );
 }
 export default Header;

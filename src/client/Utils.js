f5359a3f394ad28c19b24905aa0fc42f9eaa7588
jaimakena <jaimakena@gmail.com>
2021-10-27 22:08:28 +0200

    Correcting relative path

--- src/client/Utils.js
+++ src/client/Utils.js
@@ -2,7 +2,7 @@
 
 export function getMeals(searchText,setData) {
   const params=(searchText)?(`/${searchText}`):'';
-  const uri='http://localhost:3000/api/meals'+params;
+  const uri='/api/meals'+params;
   fetch(uri)
    .then((response) => {
      if (response.ok) {
